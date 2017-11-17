69b29005697a736d01bfc41103c4da10d9e72a06
masoudrahimi <33580434+masoudrahimi@users.noreply.github.com>
2017-11-17 16:23:03 +0330

    change model folder by Masoud

--- src/galery/model/State.java
+++ src/galery/model/State.java
@@ -4,8 +4,30 @@ package galery.model;
  * Created by masaoud on 17/11/2017.
  */
 public class State {
-    public static void main(String[] args) {
-        System.out.println("Hello Git");
+    private int id;
+    private String StateTitle;
 
+    public State() {
+    }
+
+    public State(int id, String stateTitle) {
+        this.id = id;
+        StateTitle = stateTitle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStateTitle() {
+        return StateTitle;
+    }
+
+    public void setStateTitle(String stateTitle) {
+        StateTitle = stateTitle;
     }
 }
---
+++ src/galery/model/User.java
@@ -0,0 +1,16 @@
+package galery.model;
+
+/**
+ * Created by masaoud on 17/11/2017.
+ */
+public class User {
+    private int id;
+    private String name;
+    private String family;
+    private String userName;
+    private String password;
+    private int stateId;
+    private  int questionId;
+    private String answer;
+
+}
