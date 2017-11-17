60a08bf9a852119d22512be5e63fa031cc266ac0
masaoud <masaoud64@yahoo.com>
2017-11-17 04:49:19 +0330

    First Commit By Masoud

---
+++ src/galery/model/State.java
@@ -0,0 +1,11 @@
+package galery.model;
+
+/**
+ * Created by masaoud on 17/11/2017.
+ */
+public class State {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+
+    }
+}
